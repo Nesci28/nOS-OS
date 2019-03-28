39130aa46fee6f69b89f5178ed389ebf9550c84b
markgagnon28 <markgagnon28@gmail.com>
2019-03-28 15:49:24 -0400

    fixed sudo in the commands

--- nOS/PowerController.js
+++ nOS/PowerController.js
@@ -18,24 +18,24 @@ module.exports = async function(json, step) {
     let wattCommand = ''
     if (step == "init") {
       if (json.Nvidia.GPU.length > 0) wattCommand = await initialize(json, json.Nvidia.GPU.length, "Nvidia")
-      // if (wattCommand !== '') cp.execSync(wattCommand)
+      if (wattCommand !== '') cp.execSync('sudo ' + wattCommand)
 
       if (json.Amd.GPU.length > 0) wattCommand = await initialize(json, json.Amd.GPU.length, "Amd")
       if (wattCommand !== '') {
         try {
-          cp.execSync(wattCommand)
+          cp.execSync('sudo ' + wattCommand)
         } catch{}
       }
     }
 
     if (step !== "init" && step !== "stop") {
       if (json.Nvidia.GPU.length > 0) wattCommand = await checkCurrent(json, json.Nvidia.GPU.length, "Nvidia")
-      if (wattCommand !== '') cp.execSync(wattCommand)
+      if (wattCommand !== '') cp.execSync('sudo ' + wattCommand)
 
       if (json.Amd.GPU.length > 0) wattCommand = await checkCurrent(json, json.Amd.GPU.length, "Amd")
       if (wattCommand !== '') {
         try {
-          cp.execSync(wattCommand)
+          cp.execSync('sudo ' + wattCommand)
         } catch {}
       }
     }
@@ -52,17 +52,15 @@ module.exports = async function(json, step) {
       if (brand == "Nvidia") {
         if (ocSettings[brand]["Use Hive_OC"]) {     
         let hiveOC = await ocHelper(json[brand]["GPU"][i].Name)
-          // NVIDIA
-
           if (hiveOC !== 'no DB') {
-            if (hiveOC.power_limit) initCommand += `sudo nvidia-smi -i ${i} -pl ${hiveOC.power_limit}; ` 
+            if (hiveOC.power_limit) initCommand += `nvidia-smi -i ${i} -pl ${hiveOC.power_limit}; ` 
             powerStatus["Power"][brand][i] = hiveOC.power_limit
             nextWatt = hiveOC.power_limit
           } else {
             initCommand += defaultValues(json, maxPower, i, brand, initCommand)
           }          
         }
-      } else {
+      } else if (brand == "Amd") {
         initCommand = defaultValues(json, maxPower, i, brand, initCommand)
       }
       if (nextWatt) powerStatus["Power"][brand][i] = nextWatt
@@ -74,7 +72,7 @@ module.exports = async function(json, step) {
         let minWatt = json[brand]["GPU"][i]["Min Watt"].split(' ')[0]
         let maxWatt = json[brand]["GPU"][i]["Max Watt"].split(' ')[0]
         nextWatt = Math.round(Number(minWatt) + (maxWatt - minWatt) / 50 * (maxPower - 50))
-        initCommand += `sudo nvidia-smi -i ${i} -pl ${nextWatt}; `
+        initCommand += `nvidia-smi -i ${i} -pl ${nextWatt}; `
       }
 
       if (brand == "Amd") {     
@@ -87,8 +85,11 @@ module.exports = async function(json, step) {
 
         let minWatt = (maxPower / 2).toFixed(0)
         let maxWatt = maxPower
+        
+        // check value of maxPower, minWatt and maxWatt
+        
         nextWatt = Math.round(Number(minWatt) + (maxWatt - minWatt) / 50 * (maxPower - 50))
-        initCommand += `sudo ./helpers/ROC-smi/rocm-smi -d ${i} --setpoweroverdrive ${nextWatt} --autorespond yes; `
+        initCommand += `./helpers/ROC-smi/rocm-smi -d ${i} --setpoweroverdrive ${nextWatt} --autorespond yes; `
         if (nextWatt) powerStatus["Power"][brand][i] = nextWatt
       }
 
@@ -109,11 +110,11 @@ module.exports = async function(json, step) {
       if (currentFanSpeed >= maxFanSpeed && currentTemp > ocSettingserature) {
         nextWatt = json[brand]["GPU"][i]["Watt"] - 5
         if (brand == "Nvidia") {
-          wattCommand += `sudo nvidia-smi -i ${i} -pl ${nextWatt}; `
+          wattCommand += `nvidia-smi -i ${i} -pl ${nextWatt}; `
         }
 
         if (brand == "Amd") {
-          wattCommand += `sudo ./helpers/ROC-smi/rocm-smi -d ${i} --setpoweroverdrive ${nextWatt} --autorespond yes; `
+          wattCommand += `./helpers/ROC-smi/rocm-smi -d ${i} --setpoweroverdrive ${nextWatt} --autorespond yes; `
         }
       }
       if (nextWatt) powerStatus["Power"][brand][i] = nextWatt
