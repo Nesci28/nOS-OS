c4bf048f99827f55dfec6051e98f0783fb8122cd
nesci <markgagnon28@gmail.com>
2019-03-28 21:30:29 -0400

    I think my mixer works now

--- Build/mixer.sh
+++ Build/mixer.sh
@@ -13,15 +13,11 @@ sudo rm -r .cache
 
 size=$(sudo du -cs --block-size=512 /mnt/USB | tail -1)
 size=$(echo ${size} | cut -d ' ' -f1)
-size=$((size+209609))
+size=$((size+838436))
 
 cd ~/image
 sudo dd if=/dev/zero of=~/image/nOS.img bs=512 count=${size} status=progress
-
-sudo losetup -fP nOS.img
-loopDevice=$(losetup -a | head -1 | cut -d ' ' -f1 | sed 's/://g')
-
-sudo gdisk ${loopDevice} <<EOF
+sudo gdisk nOS.img <<EOF
 o
 Y
 n
@@ -44,9 +40,13 @@ n
 
 
 8300
+p
 w
+y
 EOF
-sleep 234
+
+sudo losetup -fP nOS.img
+loopDevice=$(losetup -a | head -1 | cut -d ' ' -f1 | sed 's/://g')
 
 sudo mkfs.ext4 ${loopDevice}p4
 sudo dd if=${disk}1 of=${loopDevice}p1 bs=512 status=progress
@@ -54,13 +54,21 @@ sudo dd if=${disk}2 of=${loopDevice}p2 bs=512 status=progress
 sudo dd if=${disk}4 of=${loopDevice}p3 bs=512 status=progress
 
 sudo mkdir -p /mnt/source /mnt/destination
-sudo rsync -aHAX --info=progress2 /mnt/USB /mnt/destination
+sudo mount ${loopDevice}p4 /mnt/destination
+
+sudo rsync -aHAX --info=progress2 /mnt/USB/* /mnt/destination
 
 oldUUID=$(lsblk -oNAME,UUID ${disk}3)
-newUUID=$(lsblk -oNAME,UUID ${loopDevice}4)
+newUUID=$(lsblk -oNAME,UUID ${loopDevice}p4)
 sed "s/^UUID=${oldUUID}$/UUID=${newUUDI}/" </mnt/destination/etc/fstab
 
-sudo umount ${loopDevice}
+echo $newUUID
+cat /mnt/destination/etc/fstab
+
+sleep 234
+
+sudo umount ${loopDevice}p4
+sudo losetup -d ${loopDevice}
 
 zip nOS.zip nOS.img
 
