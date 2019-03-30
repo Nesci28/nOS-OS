5b06362369af76299788653b854f5396d60c874e
nesci <markgagnon28@gmail.com>
2019-03-30 15:04:49 -0400

    i think i am done with the mixer

--- Build/mixer.sh
+++ Build/mixer.sh
@@ -1,4 +1,4 @@
-# Dependancies: du, losetup, rsync, gdrive, zip
+# Dependancies: du, losetup, rsync, gdrive, p7zip
 
 lsblk
 read -p 'Select the disk letter /dev/sdX : ' disk1
@@ -13,7 +13,7 @@ sudo rm -r .cache
 
 size=$(sudo du -cs --block-size=512 /mnt/USB | tail -1)
 size=$(echo ${size} | cut -d ' ' -f1)
-size=$((size+838436))
+size=$((size+1000000))
 
 cd ~/image
 sudo dd if=/dev/zero of=~/image/nOS.img bs=512 count=${size} status=progress
@@ -45,8 +45,13 @@ w
 y
 EOF
 
+sleep 1
+cd ~/image
 sudo losetup -fP nOS.img
 loopDevice=$(losetup -a | head -1 | cut -d ' ' -f1 | sed 's/://g')
+if [[ -z ${loopDevice} ]]; then
+  read -p "Something went wrong, try to run \"sudo losetup -fP nOS.img\" manually" var
+fi
 
 sudo mkfs.ext4 ${loopDevice}p4
 sudo dd if=${disk}1 of=${loopDevice}p1 bs=512 status=progress
@@ -58,22 +63,51 @@ sudo mount ${loopDevice}p4 /mnt/destination
 
 sudo rsync -aHAX --info=progress2 /mnt/USB/* /mnt/destination
 
-oldUUID=$(lsblk -oNAME,UUID ${disk}3)
-newUUID=$(lsblk -oNAME,UUID ${loopDevice}p4)
-sed "s/^UUID=${oldUUID}$/UUID=${newUUDI}/" </mnt/destination/etc/fstab
+cd /mnt/destination
+sudo mount -t proc /proc proc/
+sudo mount --rbind /sys sys/
+sudo mount --rbind /dev dev/
+sudo mount --rbind /run run/
 
-echo $newUUID
-cat /mnt/destination/etc/fstab
+disk=$disk loopDevice=$loopDevice sudo -E chroot /mnt/destination /bin/bash <<"EOT"
+mount -a
+oldUUID=$(lsblk -oNAME,UUID ${disk}3 | tail -1 | cut -d ' ' -f2)
+newUUID=$(lsblk -oNAME,UUID ${loopDevice}p4 | tail -1 | cut -d ' ' -f2)
+sed -i "s/${oldUUID}/${newUUID}/g" /etc/fstab
+sed -i "s/${oldUUID}/${newUUID}/g" /boot/grub/grub.cfg
+mkinitcpio -p linux
+EOT
 
-sleep 234
+sudo cp ~/mnt/USB/home/nos/SystemConfig.json /mnt/destination/ntfs/SystemConfig.json
+sudo cp ~/mnt/USB/home/nos/CoinsConfig.json /mnt/destination/ntfs/CoinsConfig.json
+sudo cp ~/mnt/USB/home/nos/Overclocks.json /mnt/destination/ntfs/Overclocks.json
 
-sudo umount ${loopDevice}p4
+sudo umount -l /mnt/destination/boot
+sudo umount -l /mnt/destination/ntfs
+sudo umount -l /mnt/destination
 sudo losetup -d ${loopDevice}
 
-zip nOS.zip nOS.img
+cd ~/image
 
-gdrive upload nOS.zip
-sleep 5
-ID=$(gdrive list | grep nOS.img | cut -d ' ' -f1)
-sleep 5
-gdrive share ${ID}
+ID=$(gdrive list | grep nOS.zip | cut -d ' ' -f1)
+
+if [[ ! -z ${ID} ]]; then
+  response=''
+  while [[ ${response} == *"Error 403"* || -z ${response} ]]; do
+    response=$(gdrive delete ${ID})
+    sleep 1
+  done
+fi
+
+7z a nOS.zip nOS.img
+
+response=''
+while [[ ${response} == *"Error 403"* || -z ${response} ]]; do
+  response=$(gdrive upload nOS.zip)
+  sleep 1
+done
+response=''
+while [[ ${response} == *"Error 403"* || -z ${response} ]]; do
+  response=$(gdrive share -i ${ID})
+  sleep 1
+done
