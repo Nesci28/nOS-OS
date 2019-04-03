d390c772172ef9c160ce6b43ea332938b822e208
nesci <markgagnon28@gmail.com>
2019-04-03 17:24:12 -0400

    amd auto bios modder (WIP)

---
+++ nOS/amdBios.js
@@ -0,0 +1,12 @@
+module.exports = async function(json) {
+	// Dependancies
+  const cp = require('child_process');
+ 
+  let memType = cp.execSync('sudo ./helpers/amdmeminfo -s -q | cut -d \':\' -f5')
+  let name =  cp.execSync('sudo ./helpers/amdmeminfo -s -q | cut -d \':\' -f3')
+
+  for (let i = 0; i < json.Amd.GPU.length; i++) {
+    
+  }
+}
+
