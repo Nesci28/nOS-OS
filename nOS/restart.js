9dae8cad62dc19c0810710baac1b54102cace24b
nesci <markgagnon28@gmail.com>
2019-08-28 18:28:10 -0400

    restart script

---
+++ nOS/restart.js
@@ -0,0 +1,31 @@
+const cp = require('child_process');
+
+module.exports = async function restart() {
+  const startOutput = start();
+  startOutput.stdout.on("data", function (data) {
+    console.log(data.toString().trim());
+  });
+} 
+
+function start() {
+  return cp.exec(
+    `cd ${findnOS()}; DISPLAY=:0 XAUTHORITY=${findXAuthority()} ./start.sh`
+  );
+}
+
+function findnOS() {
+  return cp
+    .execSync("sudo find /home -name nOS")
+    .toString()
+    .trim();
+}
+
+function findXAuthority() {
+  return cp
+    .execSync(
+      "ps -u $(id -u) -o pid= | xargs -I{} cat /proc/{}/environ 2>/dev/null | tr '\\0' '\\n' | grep -m1 '^XAUTHORITY='"
+    )
+    .toString()
+    .trim()
+    .split("=")[1];
+}
