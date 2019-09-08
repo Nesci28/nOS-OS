c3a1924d176191be106f8520dae5236d898c94c7
nesci <markgagnon28@gmail.com>
2019-09-08 15:35:01 -0400

    new image

---
+++ nOS/xorg.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [[ $(lspci | grep VGA | tail -1) == *"NVIDIA"*  ]]; then
+	if [[ -f /etc/X11/xorg.conf.bck  ]]; then
+		sudo mv /etc/X11/xorg.conf.bck /etc/X11/xorg.conf
+	fi
+	if [[ -f /etc/X11/xorg.conf.d.bck  ]]; then
+		sudo mv /etc/X11/xorg.conf.d.bck /etc/X11/xorg.conf.d
+	fi
+	if [[ -f /etc/X11/mhwd.d/nvidia.conf.bck  ]]; then
+		sudo mv /etc/X11/mhwd.d/nvidia.conf.bck /etc/X11/mhwd.d/nvidia.conf
+	fi
+else
+	if [[ -f /etc/X11/xorg.conf  ]]; then
+		sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bck
+	fi
+	if [[ -f /etx/X11/xorg.conf.d  ]]; then
+		sudo mv /etc/X11/xorg.conf.d /etc/X11/xorg.conf.d.bck
+	fi
+	if [[ -f /etc/X11/mhwd.d/nvidia.conf  ]]; then
+		sudo mv /etc/X11/mhwd.d/nvidia.conf /etc/X11/mhwd.d/nvidia.conf.bck
+	fi
+fi
+
