64e152b3288a5c5306392d92431a4113bd1d8cb5
nesci <markgagnon28@gmail.com>
2019-10-05 07:03:48 -0400

    console log

--- nOS/getInfo.js
+++ nOS/getInfo.js
@@ -105,8 +105,6 @@ module.exports = function (step, json = "", counter) {
       }
       json["Remote GitHash"] = await remoteHash();
 
-      console.log(json["Local GitHash"].replace(/[\n\t\r]/g,""), json["Remote GitHash"].replace(/[\n\t\r]/g,""))
-
       if (json["Local GitHash"].replace(/[\n\t\r]/g,"") !== json["Remote GitHash"].replace(/[\n\t\r]/g,"")) {
         console.log("Updating nOS to the lastest version... Please wait.");
         console.log("nOS will automatically restart afterward.");
